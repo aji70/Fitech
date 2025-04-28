4a4f89b4588bf4b6c90fdd4a1cf8138f33350284
Ajidokwu Sabo <realjaiboi70@gmail.com>
2025-04-28 12:33:53 +0100

    feat: added Diamond contract scafold

--- Foundry-Hardhat-Diamonds-master-Scarfold/test/deployDiamond.t.sol
+++ Foundry-Hardhat-Diamonds-master-Scarfold/test/deployDiamond.t.sol
@@ -4,10 +4,7 @@ pragma solidity ^0.8.0;
 import "../contracts/interfaces/IDiamondCut.sol";
 import "../contracts/facets/DiamondCutFacet.sol";
 import "../contracts/facets/DiamondLoupeFacet.sol";
-import "../contracts/facets/SetterGetter.sol";
-import "../contracts/facets/SetEmail.sol";
 import "../contracts/facets/OwnershipFacet.sol";
-import "../contracts/libraries/LibAppStorage.sol";
 import "forge-std/Test.sol";
 import "../contracts/Diamond.sol";
 
@@ -17,21 +14,18 @@ contract DiamondDeployer is Test, IDiamondCut {
     DiamondCutFacet dCutFacet;
     DiamondLoupeFacet dLoupe;
     OwnershipFacet ownerF;
-    SetterGetter setter;
-    SetEmail setemail;
 
-    function setUp() public {
+    function testDeployDiamond() public {
         //deploy facets
         dCutFacet = new DiamondCutFacet();
         diamond = new Diamond(address(this), address(dCutFacet));
         dLoupe = new DiamondLoupeFacet();
         ownerF = new OwnershipFacet();
-        setter = new SetterGetter();
 
         //upgrade diamond with facets
 
         //build cut struct
-        FacetCut[] memory cut = new FacetCut[](3);
+        FacetCut[] memory cut = new FacetCut[](2);
 
         cut[0] = (
             FacetCut({
@@ -49,81 +43,12 @@ contract DiamondDeployer is Test, IDiamondCut {
             })
         );
 
-        cut[2] = (
-            FacetCut({
-                facetAddress: address(setter),
-                action: FacetCutAction.Add,
-                functionSelectors: generateSelectors("SetterGetter")
-            })
-        );
-
         //upgrade diamond
         IDiamondCut(address(diamond)).diamondCut(cut, address(0x0), "");
 
         //call a function
         DiamondLoupeFacet(address(diamond)).facetAddresses();
-        SetterGetter l = SetterGetter(address(diamond));
-        l.ChangeNameAndNumber(20, "Aji");
     }
-
-    function testSetEmail() public {
-        // deploying set Email contract
-        setemail = new SetEmail();
-
-        //build cut struct
-        FacetCut[] memory cut = new FacetCut[](1);
-
-        // setter getter prepared to be added to the diamond
-        cut[0] = (
-            FacetCut({
-                facetAddress: address(setemail),
-                action: FacetCutAction.Add,
-                functionSelectors: generateSelectors("SetEmail")
-            })
-        );
-
-        //upgrade diamond
-        //  this makes the setter getter part of the diamond
-        IDiamondCut(address(diamond)).diamondCut(cut, address(0x0), "");
-
-        //call a function
-        SetEmail l = SetEmail(address(diamond));
-        l.setEmail("Aji@gmail.com");
-        LibAppStorage.Layout memory layout = l.getLayout1();
-
-        assertEq(layout.name, "Aji");
-        assertEq(layout.number, 20);
-        assertEq(layout.email, "Aji@gmail.com");
-    }
-
-    // function testAddSetterGetterToDiamond() public {
-    //     // deploying contract with setter getter logic
-    //     setter = new SetterGetter();
-
-    //     //build cut struct
-    //     FacetCut[] memory cut = new FacetCut[](1);
-
-    //     // setter getter prepared to be added to the diamond
-    //     cut[0] = (
-    //         FacetCut({
-    //             facetAddress: address(setter),
-    //             action: FacetCutAction.Add,
-    //             functionSelectors: generateSelectors("SetterGetter")
-    //         })
-    //     );
-
-    //     //upgrade diamond
-    //     //  this makes the setter getter part of the diamond
-    //     IDiamondCut(address(diamond)).diamondCut(cut, address(0x0), "");
-
-    //     //call a function
-    //     SetterGetter l = SetterGetter(address(diamond));
-    //     l.ChangeNameAndNumber(20, "Aji");
-    //     LibAppStorage.Layout memory layout = l.getLayout();
-
-    //     assertEq(layout.name, "Aji");
-    //     assertEq(layout.number, 20);
-    // }
 
     function generateSelectors(
         string memory _facetName
